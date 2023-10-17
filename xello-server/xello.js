3443d6bcb8c2d3cd2923ace438cd3c7e018fbdd4
adanglingpointer <dave@imail.cloud>
2023-10-16 21:54:31 -0400

    capture IPs

--- xello-server/xello.js
+++ xello-server/xello.js
@@ -11,8 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 let targetPleskVersionName;
 let targetPleskVersionNumber;
-var currentVersionName = "wtf";
-var currentVersionNumber = "wtf";
+var currentVersionName = "undefined";
+var currentVersionNumber = "undefined";
 
 app.post("/", async (request, response) => {
   const requestData = request.query.data;
@@ -66,8 +66,11 @@ const pleskVersionMatchFunction = async () => {
   return axios
     .get("https://docs.plesk.com/release-notes/obsidian/change-log/")
     .then(async (response) => {
+      // let pleskReleaseRegex = new RegExp(
+      //   `changelog-entry__title"\>Plesk (?<versionName>\\w+) (?<versionNumber>[\\d{1,3}\.]{2}\.\\d+)`
+      // );
       let pleskReleaseRegex = new RegExp(
-        `changelog-entry__title"\>Plesk (?<versionName>\\w+) (?<versionNumber>[\\d{1,3}\.]{2}\.\\d+)`
+        `changelog-entry__title"\>Plesk (?<versionName>\\w+) (?<versionNumber>\\d{1,3}[.]\\d{1,3}[.]\\d{1,3})`
       );
       //<h2 class="changelog-entry__title">Plesk Obsidian 18.0.56
       let currentVersionMatch = await pleskReleaseRegex.exec(response.data);
@@ -102,9 +105,10 @@ const scanDomain = async (domain) => {
 
   // find out versioning of Plesk
   if (pleskRegex.test(returnedScan)) {
+    // if Plesk port is open
     let pleskCheck = await pleskScan(domain);
     let pleskVersionRegex = new RegExp(
-      `(?:http-title:\\sPlesk\\s)(?<versionName>\\w+)\\s(?<versionNumber>[\\d{1,3}\.]{2}\.\\d+)`
+      `(?:http-title:\\sPlesk\\s)(?<versionName>\\w+)\\s(?<versionNumber>\\d{1,3}[.]\\d{1,3}[.]\\d{1,3})`
     );
     //http-title: Plesk Obsidian 18.0.55
     if (pleskVersionRegex.test(pleskCheck)) {
@@ -122,8 +126,53 @@ const scanDomain = async (domain) => {
       console.log("currentVersionName " + currentVersionName);
     }
   } else {
+    // Plesk port not open
     targetPleskVersionName = "undetected";
     targetPleskVersionNumber = "undetected";
+  }
+
+  // match IP addresses
+  // Nmap scan report for unlimitedweb.space (74.208.157.83)
+  let ipMatchingRegex = new RegExp(
+    `Nmap scan report for ${domain} \\((?<ipAddress>\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})\\)`
+  );
+  if (ipMatchingRegex.test(returnedScan)) {
+    // we've found an IPv4
+    let ipMatch = ipMatchingRegex.exec(returnedScan);
+    var primaryIpAddress = ipMatch.groups.ipAddress;
+    //console.log(primaryIpAddress);
+  } else {
+    // no IPv4 found
+    var primaryIpAddress = "undefined";
+  }
+  // Other addresses for unlimitedweb.space (not scanned): 2607:f1c0:1800:239::1
+  // Other addresses for yahoo.com (not scanned): 98.137.11.164 74.6.143.25 2001:4998:44:3507::8000 2001:4998:124:1507::f001
+  let secondaryIpMatchingRegex = new RegExp(
+    `Other addresses for ${domain} \\(not scanned\\): (?<secondaryIps>.+\\b)`
+  );
+  if (secondaryIpMatchingRegex.test(returnedScan)) {
+    // found secondary IPs
+    let secondaryIpMatch = secondaryIpMatchingRegex.exec(returnedScan);
+    var secondaryIpAddresses = secondaryIpMatch.groups.secondaryIps;
+    //console.log(secondaryIpAddresses);
+  } else {
+    // no secondary IPs found
+    var secondaryIpAddresses = "undefined";
+  }
+
+  // console.log("primaryIpAddress outside.. " + primaryIpAddress);
+  // console.log("secondaryIpAddresses outside "+secondaryIpAddresses);
+
+  /* TODO: Fix regex for rDNS */
+
+  // Find rDNS
+  // rDNS record for 74.6.143.26: media-router-fp74.prod.media.vip.bf1.yahoo.com
+  let rdnsRegex = new RegExp(`rDNS record for .+\\b(?<rdnsRecord>.+)[\\b]`);
+  if (rdnsRegex.test(returnedScan)) {
+    // rDNS address found
+    let rdnsMatch = rdnsRegex.exec(returnedScan);
+    var ptrRecord = rdnsMatch.groups.rdnsRecord;
+    console.log(ptrRecord);
   }
 
   return {
@@ -132,6 +181,9 @@ const scanDomain = async (domain) => {
     targetPleskVersion: targetPleskVersionNumber,
     currentPleskName: currentVersionName,
     currentPleskVersion: currentVersionNumber,
+    domainMainIp: primaryIpAddress,
+    domainSecondaryIps: secondaryIpAddresses,
+    reverseDNS: ptrRecord,
     anotherValue: "1",
   };
 };
